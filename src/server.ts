dc494e98525489245c534c810d7a0ab64fddae78
Udayon-Paul-Dhrubo <1805109@ugrad.cse.buet.ac.bd>
2023-06-18 00:13:13 +0600

    error-handling

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import express from 'express'
 import { Request, Response } from "express";
+import { notFoundHandler, defaultErrorHandler } from "./error-handler/error-handler";
 
 const app = express();
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Application works!");
 });
+
+//404 handler
+app.use(notFoundHandler);
+
+//default error handler
+app.use(defaultErrorHandler);
 
 app.listen(3000, () => {
   console.log("Application started on port 3000!");
